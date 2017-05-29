9d1077e7d923d906ef6eb2eb6b7586a45cede69d
Karina <karibl36@gmail.com>
2017-05-29 10:32:12 -0600

    Template

---
+++ Base/footer.php
@@ -0,0 +1,6 @@
+
+
+<div id="content_footer"></div>
+    <div id="footer">
+      Copyright &copy; | <a href="https://www.ucr.ac.cr/">Universidad de Costa Rica</a>
+</div>
---
+++ Base/header.php
@@ -0,0 +1,20 @@
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="../View/index.php">Turis<span class="logo_colour">Tico.com</span></a></h1>
+          <h2>Recomendaciones de Sitios Turísticos</h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="selected"><a href="../View/index.php">Home</a></li>
+          <li><a href="">Recomendaciones</a></li>
+          <li><a href="">Perfil</a></li>
+          <li><a href="../View/Contact.php">Contáctenos</a></li>
+        </ul>
+      </div>
+    </div>
